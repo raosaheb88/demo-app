7e242ad2a9dbb9ac9ed73c3ee4406f3d133f0e6c
Raosaheb Deshmukh <raosaheb.deshmukh5710@gmail.com>
2026-06-20 16:33:49 +0530

    added sout and username and pwd

--- src/main/java/com/example/demo_app/controller/HelloController.java
+++ src/main/java/com/example/demo_app/controller/HelloController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    private String username = "RD5710";
+    private String password = "rd12345";
     
     @GetMapping("/hello")
     public String sayHello() {
+        System.out.println("HelloController: sayHello() called");
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
         return "Hello, World!";
     }
 
