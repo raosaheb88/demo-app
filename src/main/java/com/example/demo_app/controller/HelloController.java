0dd66094f469c79f8b1cfe64736cd95a81406daf
Raosaheb Deshmukh <raosaheb.deshmukh5710@gmail.com>
2026-06-21 12:07:04 +0530

    revert changes done

--- src/main/java/com/example/demo_app/controller/HelloController.java
+++ src/main/java/com/example/demo_app/controller/HelloController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
-
-    private final String password = "rd598741";
-
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
