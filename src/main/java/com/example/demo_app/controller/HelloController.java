ba50f04f6cc92660897bbe3f92df5cf9dc0512e3
Raosaheb Deshmukh <raosaheb.deshmukh5710@gmail.com>
2026-06-21 11:19:05 +0530

    update github yml file

--- src/main/java/com/example/demo_app/controller/HelloController.java
+++ src/main/java/com/example/demo_app/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.demo_app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.TimeZone;
-
 @RestController
+@RequestMapping("/api")
 public class HelloController {
+
+
+    private final String password = "rd598741";
     
     @GetMapping("/hello")
     public String sayHello() {
